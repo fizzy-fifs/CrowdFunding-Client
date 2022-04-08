9f05e1a99f6903c5f484810e196bc99ae4b92d5a
Fifolu Akinyemi <fkinyemi@gmail.com>
2022-04-08 18:33:42 +0100

    Change category api call to point to deployed backend server

--- src/apiCalls/fetchCategories.js
+++ src/apiCalls/fetchCategories.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchCategories = async () => {
   const data = await axios
-    .get("http://localhost:8080/api/v1.0/projects/getcategories")
+    .get("https://fundedlocal-server.herokuapp.com/api/v1.0/projects/getcategories")
     .then((res) => res.data);
 
   return data;
